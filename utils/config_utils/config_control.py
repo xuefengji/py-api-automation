eb9b188d30d0426ad8f66c69f1a7884a18bcde02
@xuefeng.ji <xuefvip@163.com>
2022-09-06 13:35:32 +0800

    refactor(utils): refactoring config class

--- utils/config_utils/config_control.py
+++ utils/config_utils/config_control.py
@@ -11,43 +11,39 @@ from utils.file_utils.operation_yaml import OperationYaml
 
 class ConfigGet(BaseConfig):
 
-    # 获取环境地址
-    @classmethod
-    def get_environment(cls, environment: str) -> dict:
+    def __init__(self):
+        self.yaml_data = OperationYaml.read_yaml(ConfigGet.setting_dir)
+
+
+    def get_environment(self, environment: str) -> dict:
         """
         获取相应环境信息
         : param environment: setting 中配置的环境信息
         """
-        env = OperationYaml.read_yaml(cls.environment_dir)
+        env = OperationYaml.read_yaml(self.environment_dir)
         if environment in env.keys():
             return env[environment]
         raise ValueError("配置的环境信息不存在")
 
-    # 获取setting配置
-    @classmethod
-    def get_setting(cls) -> dict:
-        setting = OperationYaml.read_yaml(cls.setting_dir)
-        return setting
-
-    # 获取主机host
-    @classmethod
-    def get_host(cls) -> str:
+    #获取主机信息
+    def get_host(self) -> str:
         # print(cls.setting_dir)
-        environment = cls.get_setting()['environment']
-        return cls.get_environment(str(environment))['host']
+        environment = self.yaml_data['environment']
+        return self.get_environment(str(environment))['host']
 
     #获取 email 信息
-    @classmethod
-    def get_email(cls) -> dict:
-        return cls.setting_dir['email']
 
-    @classmethod
-    def get_mysql(cls) -> dict:
-        return cls.setting_dir['mysql']
+    def get_email(self) -> dict:
+        return self.yaml_data['email']
 
 
-if __name__ == '__main__':
-    data = ConfigGet.get_host()
-    print(data)
+    def get_mysql(self) -> dict:
+        return self.yaml_data['mysql']
+
+
+    def get_ding_talk(self) -> dict:
+        return self.yaml_data['ding_talk']
+
+
 
 
