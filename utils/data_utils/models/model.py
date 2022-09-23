22d20954d44327e44e23e0a197afc7d366238e49
@xuefeng.ji <xuefvip@163.com>
2022-09-23 14:19:14 +0800

    feat(utils): add model.py

---
+++ utils/data_utils/models/model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2022/9/23
+# @Author: xuef
+# @File: model.py
+# @Desc:
+
+import types
+from typing import Text, Dict, Callable, Union, Optional, List, Any
+from dataclasses import dataclass
+from pydantic import BaseModel, Field
+
