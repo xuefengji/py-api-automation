449ea29bebadb404f9b8f3665b62f7c36d98e245
@xuefeng.ji <xuefvip@163.com>
2022-09-27 18:05:15 +0800

    feat(utils): add model.py

---
+++ utils/model_utils/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2022/9/27
+# @Author: xuef
+# @File: __init__.py.py
+# @Desc:
---
+++ utils/model_utils/model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2022/9/27
+# @Author: xuef
+# @File: model.py
+# @Desc:
+
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
