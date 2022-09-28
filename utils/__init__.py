9e14169394f5e0c9f97cb951a536b03058c5921c
@xuefeng.ji <xuefvip@163.com>
2022-09-28 09:59:09 +0800

    feat(utils): add get config data

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,11 @@
 # @Author: xuef
 # @File: __init__.py
 # @Desc:
+
+from utils.data_utils.models.model import Config
+from utils.file_utils.operation_yaml import OperationYaml
+from config import BaseConfig
+
+config_data = OperationYaml.read_yaml(BaseConfig.setting_dir)
+config = Config(**config_data)
+
