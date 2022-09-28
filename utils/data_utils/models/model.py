c2c127a71fa11b32126aa42bd9a78a6c8c31fe17
@xuefeng.ji <xuefvip@163.com>
2022-09-28 09:58:56 +0800

    feat(utils): update data_utils/model.py

--- utils/data_utils/models/model.py
+++ utils/data_utils/models/model.py
@@ -5,8 +5,45 @@
 # @File: model.py
 # @Desc:
 
-import types
-from typing import Text, Dict, Callable, Union, Optional, List, Any
-from dataclasses import dataclass
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+from typing import Union, List
+
+
+class Host(BaseModel):
+    test: str
+    proc: str
+
+
+class MySqlConf(BaseModel):
+    host: str
+    port: int = 3306
+    user: str
+    password: str
+    database: str
+    charset: str = 'utf8'
+
+
+class EmailConf(BaseModel):
+    host: str
+    user: str
+    key: str
+    recevies: Union[str, List[str]]
+
+
+class DingTalkConf(BaseModel):
+    webhook: str
+    secret: str
+
+
+class Config(BaseModel):
+    project_name: str
+    tester: str
+    env: str
+    host: Host
+    notification_type: int = 0
+    cache_type: int = 0
+    mysql: MySqlConf
+    email: EmailConf
+    ding_talk: DingTalkConf
+
 
