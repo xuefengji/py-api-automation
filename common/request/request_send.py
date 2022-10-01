08d4b33fc6d588a456b751fb0e7d194ecb4ad183
xuefengji <xuefvip@163.com>
2022-10-01 21:24:19 +0800

    refactor(common): update params name

--- common/request/request_send.py
+++ common/request/request_send.py
@@ -37,7 +37,7 @@ class RequestHandle(BaseRequest, RequestType):
                     method=self._case_data.info.method,
                     url=self._case_data.info.url,
                     headers=self._case_data.case.headers,
-                    data=self._case_data.case.data.body,
+                    data=self._case_data.case.data.data,
                     params=self._case_data.case.data.params,
                     request_type=self._case_data.case.request_type,
                 )
@@ -45,7 +45,7 @@ class RequestHandle(BaseRequest, RequestType):
                     allure.attach(name="当前请求url：", body=self._case_data.info.url)
 
                     allure.attach(name="当前请求headers：", body=str(self._case_data.case.headers))
-                    allure.attach(name="当前请求数据：", body=str(self._case_data.case.data.body))
+                    allure.attach(name="当前请求数据：", body=str(self._case_data.case.data.data))
                     allure.attach(name="当前请求结果：", body=str(res.status_code))
                 return res
             raise ValueError("当前请求方式不存在，请检查")
