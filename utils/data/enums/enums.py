8e61b2a3e563622ea492b5a3345ca7cb8380c846
@xuefeng.ji <xuefvip@163.com>
2022-10-09 14:12:41 +0800

    refactor(utils): update enums.py

---
+++ utils/data/enums/enums.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2022/8/17
+# @Author: xuef
+# @File: enums.py
+# @Desc:
+
+
+from enum import Enum, unique
+
+@unique
+class AssertEnum(Enum):
+    equals = "=="
+    less_than = "lt"
+    less_than_or_equals = "le"
+    greater_than = "gt"
+    greater_than_or_equals = "ge"
+    not_equals = "not_eq"
+    contains = "contains"
+
+@unique
+class DependsType(Enum):
+    RESPONSE = 'response'
+    REQUEST = 'request'
+    SQLDATA = 'sql_data'
+    CACHE = 'cache'
+
+@unique
+class CaseEnum(Enum):
+    URL = 'url'
+    METHOD = 'method'
+    IS_RUN = 'is_run'
+    TITLE = 'title'
+    HEADERS = 'headers'
+    COOKIES = 'cookies'
+    IS_DEPEND = 'is_depend'
+    DEPENDS_DATA = 'depends_data'
+    REQUEST_TYPE = 'request_type'
+    data = 'data'
+    ENCODE = 'encode'
+
+@unique
+class RequestTypeEnum(Enum):
+    JSON = "json"
+    PARAMS = "params"
+    FILE = "file"
+    DATA = "data"
